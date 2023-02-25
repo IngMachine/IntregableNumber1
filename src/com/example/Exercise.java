0d361e7b96a4f6d5f2259e423003565ba5e104e5
Fredy Rivera <riverafredy1997@gmail.com>
2023-02-25 15:43:33 -0500

    variable type practice exercise

---
+++ src/com/example/Exercise.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Exercise {
+    public static void main(String[] args) {
+        int number = 0;
+        long number2 = 0L;
+        double numberDouble = 0.0;
+        boolean valueBoolean = false;
+        String valueString = "value";
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(numberDouble);
+        System.out.println(valueBoolean);
+        System.out.println(valueString);
+    }
+
+
+}
